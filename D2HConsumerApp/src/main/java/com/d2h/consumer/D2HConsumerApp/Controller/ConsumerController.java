a554f8c8d5e7932aea69f19bfa3417aa90f831e7
kowsalyabe19 <kowsalyadpm98@gmail.com>
2022-03-24 12:13:58 +0530

    added HttpMthods

--- D2HConsumerApp/src/main/java/com/d2h/consumer/D2HConsumerApp/Controller/ConsumerController.java
+++ D2HConsumerApp/src/main/java/com/d2h/consumer/D2HConsumerApp/Controller/ConsumerController.java
@@ -3,15 +3,18 @@ package com.d2h.consumer.D2HConsumerApp.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.d2h.consumer.D2HConsumerApp.Entity.Consumer;
 import com.d2h.consumer.D2HConsumerApp.Repository.ConsumerRepository;
-import com.in28minutes.springboot.model.Question;
+
 
 @RestController
 public class ConsumerController {
@@ -28,9 +31,27 @@ public class ConsumerController {
 	}
 	
 	@PostMapping("/consumers")
-	public Consumer addConsumerDetails(@RequestBody Consumer cons){
-				repository.save(cons);
-		return cons;
+	public List<Consumer> addConsumerDetails(@RequestBody List<Consumer> consumers){
+				repository.saveAll(consumers);
+		       return consumers;
 		
 	}
+	@DeleteMapping("/consumers/{id}")
+	 public String deleteConsumer(@PathVariable int id) {
+		repository.deleteById(id);
+		return "deleted";
+	}
+	
+	@PutMapping("/consumers/{id}")
+	public Consumer updateConsumer(@RequestBody Consumer consumer) {
+		repository.save(consumer);
+		return consumer;
+	}
+	@PatchMapping("/consumers/{id}")
+	public Consumer partialupdateConsumer(@RequestBody Consumer consumer) {
+		repository.save(consumer);
+		return consumer;
+	}
+	
+	
 }
